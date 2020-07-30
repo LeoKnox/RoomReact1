2132eb879411c11cd6faab6b2bbf14e789e3d3e4
LeoKnox <LeoKnox@users.noreply.github.com>
2020-07-30 13:47:12 -0700

    Add files via upload

--- Data/Services/IRoomService.cs
+++ Data/Services/IRoomService.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using ReactRoom.Data.Models;
+using System.Collections.Generic;
+
+namespace ReactRoom.Data
+{
+    public interface IRoomService
+    {
+        List<Room> GetAllRooms();
+        Room GetRoomById(int roomId);
+        void updateRoom(int roomId, Room room);
+        void DeleteRoom(int roomId);
+        void AddRoom(Room room);
+    }
+}
