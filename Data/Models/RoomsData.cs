9fa1e85d17223d9ba661227441a7b8ff03e21c72
LeoKnox <LeoKnox@users.noreply.github.com>
2020-07-30 13:03:36 -0700

    Add files via upload

---
+++ Data/Models/RoomsData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactRoom.Data.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Floor { get; set; }
+        public string Wall { get; set; }
+        public int Width { get; set; }
+        public int Length { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
