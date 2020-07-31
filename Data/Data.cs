4f60bae4948d8a4b91f9fe2dc9b1ac966a132344
LeoKnox <LeoKnox@users.noreply.github.com>
2020-07-30 20:43:26 -0700

    Add files via upload

---
+++ Data/Data.cs
@@ -0,0 +1,15 @@
+﻿using ReactRoom.Data.Models;
+using System.Collections.Generic;
+
+namespace ReactRoom.Data
+{
+    public static class Data
+    {
+        public static List<Room> Rooms => allRooms;
+
+        static List<Room> allRooms = new List<Room>()
+        {
+
+        }
+    }
+}
--- Data/Services/RoomService.cs
+++ Data/Services/RoomService.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using ReactRoom.Data.Models;
+using System.Collections.Generic;
+
+namespace ReactRoom.Data
+{
+    public class RoomService : IRoomService
+    {
+        public void AddRoom(Room room)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteRoom(int roomId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Room> GetAllRooms()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Room GetRoomById(int roomId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void updateRoom(int roomId, Room room)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
