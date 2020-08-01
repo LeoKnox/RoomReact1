5e0b4cdff711cb94f4afe81fa72cf6fcb82eeb26
LeoKnox <LeoKnox@users.noreply.github.com>
2020-07-31 20:04:44 -0700

    Add files via upload

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ReactRoom.Data;
 
 namespace ReactRoom
 {
@@ -28,6 +29,8 @@ namespace ReactRoom
             {
                 configuration.RootPath = "ClientApp/build";
             });
+
+            services.AddTransient<IRoomService, RoomService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
