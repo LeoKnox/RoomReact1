82f471eb799042fd310da35c3eda683832578a80
LeoKnox <LeoKnox@users.noreply.github.com>
2020-07-31 19:28:29 -0700

    Add files via upload

--- Data/Data.cs
+++ Data/Data.cs
@@ -9,7 +9,17 @@ namespace ReactRoom.Data
 
         static List<Room> allRooms = new List<Room>()
         {
-
-        }
+            new Room()
+            {
+                Id=1,
+                Name="Entry",
+                Floor="Entry",
+                Wall="Entry",
+                Width=5,
+                Length=5,
+                DateCreated = new System.DateTime(2020, 07, 31),
+                DateModified = new System.DateTime(2020, 07, 31)
+            }
+        };
     }
 }
