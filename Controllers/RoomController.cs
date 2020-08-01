40fa9ce129f232fc4b3bcbbdfac1f9b7ed4cb8bf
LeoKnox <LeoKnox@users.noreply.github.com>
2020-07-31 20:47:56 -0700

    Add files via upload

---
+++ Controllers/RoomController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReactRoom.Data;
+using ReactRoom.Data.Models;
+
+namespace ReactRoom.Controllers
+{
+    [Route("api/[controller]")]
+    public class RoomController : Controller
+    {
+        private IRoomService _service;
+        public RoomController(IRoomService service)
+        {
+            this._service = service;
+        }
+
+        [HttpPost("AddRoom")]
+        public IActionResult AddRoom([FromBody]Room room)
+        {
+            if(room != null)
+            {
+                _service.AddRoom(room);
+            }
+            return Ok();
+        }
+    }
+}
